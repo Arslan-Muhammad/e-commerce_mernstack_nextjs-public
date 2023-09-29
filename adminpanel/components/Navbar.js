e7953698d53f6540a81f838900926ecc819cace9
Muhammad Zahid faiz <mrzahidfaiz@gmail.com>
2023-09-29 23:49:09 +0500

    improve search in product page

--- adminpanel/components/Navbar.js
+++ adminpanel/components/Navbar.js
@@ -96,25 +96,6 @@ const Navbar = () => {
                             </form>
                         </div>
                         <div className="flex items-center lg:order-2">
-                            <Link
-                                href="/products/submit"
-                                className="hidden sm:inline-flex items-center justify-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-xs px-3 py-1.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
-                            >
-                                <svg
-                                    aria-hidden="true"
-                                    className="mr-1 -ml-1 w-5 h-5"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        fillRule="evenodd"
-                                        d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                                        clipRule="evenodd"
-                                    />
-                                </svg>
-                                Add product
-                            </Link>
                             <button
                                 id="toggleSidebarMobileSearch"
                                 type="button"
--- adminpanel/pages/products/index.js
+++ adminpanel/pages/products/index.js
@@ -4,6 +4,7 @@ import { ExclamationCircleFilled } from '@ant-design/icons';
 import { Modal } from 'antd';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
+import Link from 'next/link';
 
 const { confirm } = Modal;
 
@@ -15,16 +16,22 @@ const index = ({ data }) => {
 
   // search
   const [searchQuery, setSearchQuery] = useState('');
-  const filteredProducts = allProducts.filter((product) =>
-    product.title.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  const multipleSearch = allProducts.filter((product) => 
+    Object.keys(product).some((parameter) => 
+    product[parameter].toString().toLowerCase().includes(searchQuery)
+    )
+    )
+
+    console.log(multipleSearch, 'sea')
+
+    
 
   // pagination
   const [currentPageX, setCurrentPageX] = useState(1);
   const recordsPerPage = 5;
   const lastIndex = currentPageX * recordsPerPage;
   const firstIndex = lastIndex - recordsPerPage;
-  const records = filteredProducts.slice(firstIndex, lastIndex)
+  const records = multipleSearch.slice(firstIndex, lastIndex)
   const npages = Math.ceil(allProducts.length / recordsPerPage);
   const numbers = [...Array(npages + 1).keys()].slice(1);
 
@@ -69,8 +76,6 @@ const index = ({ data }) => {
     }
   }
 
-
-
   return (
     <main className='p-4 sm:ml-64'>
       {/* Filter, Search, Sort */}
@@ -110,8 +115,8 @@ const index = ({ data }) => {
               </form>
             </div>
             <div className="flex flex-col items-stretch justify-end flex-shrink-0 w-full space-y-2 md:w-auto md:flex-row md:space-y-0 md:items-center md:space-x-3">
-              <button
-                type="button"
+              <Link
+                href="/products/submit"
                 className="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
               >
                 <svg
@@ -128,55 +133,8 @@ const index = ({ data }) => {
                   />
                 </svg>
                 Add product
-              </button>
+              </Link>
               <div className="flex items-center w-full space-x-3 md:w-auto">
-                <button
-                  id="actionsDropdownButton"
-                  data-dropdown-toggle="actionsDropdown"
-                  className="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg md:w-auto focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                  type="button"
-                >
-                  <svg
-                    className="-ml-1 mr-1.5 w-5 h-5"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                    aria-hidden="true"
-                  >
-                    <path
-                      clipRule="evenodd"
-                      fillRule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    />
-                  </svg>
-                  Actions
-                </button>
-                <div
-                  id="actionsDropdown"
-                  className="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
-                >
-                  <ul
-                    className="py-1 text-sm text-gray-700 dark:text-gray-200"
-                    aria-labelledby="actionsDropdownButton"
-                  >
-                    <li>
-                      <a
-                        href="#"
-                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      >
-                        Mass Edit
-                      </a>
-                    </li>
-                  </ul>
-                  <div className="py-1">
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                    >
-                      Delete all
-                    </a>
-                  </div>
-                </div>
                 <button
                   id="filterDropdownButton"
                   data-dropdown-toggle="filterDropdown"
@@ -227,28 +185,27 @@ const index = ({ data }) => {
                       <input
                         id="apple"
                         type="checkbox"
-                        defaultValue=""
+                       
                         className="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
                       />
                       <label
                         htmlFor="apple"
                         className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100"
                       >
-                        Apple (56)
+                        Laptop
                       </label>
                     </li>
                     <li className="flex items-center">
                       <input
                         id="fitbit"
                         type="checkbox"
-                        defaultValue=""
                         className="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
                       />
                       <label
                         htmlFor="fitbit"
                         className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100"
                       >
-                        Fitbit (56)
+                       PC
                       </label>
                     </li>
                     <li className="flex items-center">
@@ -262,7 +219,7 @@ const index = ({ data }) => {
                         htmlFor="dell"
                         className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100"
                       >
-                        Dell (56)
+                        Console
                       </label>
                     </li>
                     <li className="flex items-center">
@@ -277,7 +234,7 @@ const index = ({ data }) => {
                         htmlFor="asus"
                         className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100"
                       >
-                        Asus (97)
+                        Smart Phone
                       </label>
                     </li>
                   </ul>
